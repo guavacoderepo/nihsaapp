ce306269632452933516a20bf492f19fb51e4ae6
guavacoderepo <guavacodeplus@gmail.com>
2023-04-30 02:20:44 +0100

    location update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "95d5f8a97684ac3c3e2496f74ac4a2f6",
-"index.html": "8fcf10a2bc4c49a524d9f5f6d0ee9dd2",
-"/": "8fcf10a2bc4c49a524d9f5f6d0ee9dd2",
-"main.dart.js": "af24874f13d15e8f93ce5699073bfaa1",
+"index.html": "0451b81d8706d98f2fb1d459d1b3213e",
+"/": "0451b81d8706d98f2fb1d459d1b3213e",
+"main.dart.js": "01f3f123bc0a91cfd8c1b4a34b076132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
